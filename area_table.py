be51a1db0852f19925fab08fd160d83c6cddc77d
Issa Rice <riceissa@gmail.com>
2017-02-11 15:16:21 -0800

    Move to util

--- area_table.py
+++ area_table.py
@@ -3,14 +3,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from math import log10, floor
 
-def percentage_round(percentage):
-    if percentage > 1:
-        percentage = round(percentage)
-    else:
-        percentage = round(percentage, -int(floor(log10(abs(percentage)))))
-    return percentage
+from util import *
 
 df = pd.read_csv('grants.tsv', sep='\t')
 areas = df.area.unique()
@@ -19,7 +13,8 @@ print('{| class="sortable wikitable"')
 print('! Focus area !! Amount !! Recipients !! Percentage of total amount')
 for area in areas:
     print('|-')
-    rs = ", ".join(df[df.area == area].recipient.unique().tolist())
+    rs = ", ".join(wikilink(x)
+            for x in df[df.area == area].recipient.unique().tolist())
     amt = df.groupby('area')['amount'].sum()[area]
     p = amt / total * 100
     print('| {} || {:,d} || {} || {}'.format(area, amt, rs, percentage_round(p)))
---
+++ util.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from math import log10, floor
+
+def percentage_round(percentage):
+    if percentage > 1:
+        percentage = round(percentage)
+    else:
+        percentage = round(percentage, -int(floor(log10(abs(percentage)))))
+    return percentage
+
+def wikilink(title):
+    return "[[" + title + "]]"
+
