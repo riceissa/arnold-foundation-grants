d4f05376fc138a31386a31a8be7ddf597595fc73
Issa Rice <riceissa@gmail.com>
2017-02-11 15:33:06 -0800

    Try year table

--- year_table.py
+++ year_table.py
@@ -6,4 +6,20 @@ import matplotlib.pyplot as plt
 
 from util import *
 
+df = pd.read_csv('grants.tsv', sep='\t')
+years = df.year.sort_values().unique()
+total = df.amount.sum()
+print('{| class="sortable wikitable"')
+print('! Year !! Amount !! Recipients !! Percentage of total amount')
+for year in years:
+    print('|-')
+    rs = ", ".join(wikilink(x)
+            for x in df[df.year == year].recipient.unique().tolist())
+    amt = df.groupby('year')['amount'].sum()[year]
+    p = amt / total * 100
+    print('| {} || {:,d} || {} || {}'.format(year, amt, rs, percentage_round(p)))
+print('|-')
+print("! Total amount granted || {:,d} || || 100%".format(total))
 
+# print(df.groupby('year')['amount'].sum())
+print('|}')
