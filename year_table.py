202681535116c362640c37942d89fed88d5581d1
Issa Rice <riceissa@gmail.com>
2017-02-11 19:11:02 -0800

    Collapse long list of recipients

--- year_table.py
+++ year_table.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 
 from util import *
 
+num_outside_collapse = 10
+
 df = pd.read_csv('grants.tsv', sep='\t')
 years = df.year.sort_values().unique()
 total = df.amount.sum()
@@ -13,11 +15,22 @@ print('{| class="sortable wikitable"')
 print('! Year !! Amount !! Recipients !! Percentage of total amount')
 for year in years:
     print('|-')
-    rs = ", ".join(wikilink(x)
-            for x in df[df.year == year].recipient.unique().tolist())
+    rs = df[df.year == year].sort_values(by='amount').recipient.unique().tolist()
     amt = df.groupby('year')['amount'].sum()[year]
     p = amt / total * 100
-    print('| {} || {:,d} || {} || {}'.format(year, amt, rs, percentage_round(p)))
+    print('| {} '.format(year))
+    print('| {:,d}'.format(amt))
+    if len(rs) > 10:
+        top = rs[:num_outside_collapse]
+        rest = rs[num_outside_collapse:]
+        print('| {}'.format(", ".join(wikilink(x) for x in top))
+                + "{{collapse|"
+                + ", ".join(wikilink(x) for x in rest)
+                + "|More recipients}}"
+        )
+    else:
+        print('| {}'.format(", ".join(wikilink(x) for x in rs)))
+    print('| {}'.format(percentage_round(p)))
 print('|-')
 print("! Total amount granted || {:,d} || || 100%".format(total))
 print('|}')
