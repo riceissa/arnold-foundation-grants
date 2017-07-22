bbf8cac05fa410504207a094fbee73a8cc70b96b
Issa Rice <riceissa@gmail.com>
2017-07-22 11:25:58 -0700

    Skip headerline without temp var

--- loop.py
+++ loop.py
@@ -68,27 +68,24 @@ print("""insert into donations (donor, donee, amount, donation_date,
     donor_cause_area_url, notes, affected_countries, affected_states) values""")
 
 with open("grants-with-multiyear.tsv", "r") as f:
-    headerLine = True
+    next(f)  # skip header line of tsv
     continuing = False
     for line in f:
-        if headerLine:
-            headerLine = False
+        if continuing:
+            print(",")
         else:
-            if continuing:
-                print(",")
-            else:
-                continuing = True
-            area, recipient, year, amount, donation_date_precision, notes = line.strip().split("\t")
-            if recipient in DONEE_RENAME:
-                recipient = DONEE_RENAME[recipient]
-            print("""    ("Laura and John Arnold Foundation","{donee}",{amount},"{donation_date}-01-01","{donation_date_precision}","donation log","{cause_area}","http://www.arnoldfoundation.org/grants/",{donor_cause_area_url},"{notes}","United States",{affected_states})""".format(
-                donee=donee_clean(recipient),
-                amount=amount,
-                donation_date=year,
-                donation_date_precision=donation_date_precision,
-                cause_area=area,
-                donor_cause_area_url=donor_cause_area_url(area),
-                notes=notes,
-                affected_states=assign_state(recipient)
-            ), end='')
+            continuing = True
+        area, recipient, year, amount, donation_date_precision, notes = line.strip().split("\t")
+        if recipient in DONEE_RENAME:
+            recipient = DONEE_RENAME[recipient]
+        print("""    ("Laura and John Arnold Foundation","{donee}",{amount},"{donation_date}-01-01","{donation_date_precision}","donation log","{cause_area}","http://www.arnoldfoundation.org/grants/",{donor_cause_area_url},"{notes}","United States",{affected_states})""".format(
+            donee=donee_clean(recipient),
+            amount=amount,
+            donation_date=year,
+            donation_date_precision=donation_date_precision,
+            cause_area=area,
+            donor_cause_area_url=donor_cause_area_url(area),
+            notes=notes,
+            affected_states=assign_state(recipient)
+        ), end='')
     print(";")
